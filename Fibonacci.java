cf1a0cfc1367852292d2b3014f2b57ce95e2a5fc
peterpileta <escallobro@gmail.com>
2025-01-17 19:07:56 +0000

    Create Fibonacci.java

---
+++ Fibonacci.java
@@ -0,0 +1,16 @@
+public class Fibonacci {
+
+    // Recursive method to return the nth term of the Fibonacci sequence
+    public static int fibonacciRecursive(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacciRecursive(n - 1) + fibonacciRecursive(n - 2);
+    }
+
+    public static void main(String[] args) {
+        int n = 10;
+        int result = fibonacciRecursive(n);
+        System.out.println("The " + n + "th term of the Fibonacci sequence is " + result + ".");
+    }
+}
